5a2e31a3dad8142c9708fa64f11bd008b14f379d
Jose <jose_roberto_6@msn.com>
2022-11-21 02:27:32 -0300

    a04

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,10 +1,12 @@
 import { VStack } from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { MagnifyingGlass } from "phosphor-react-native";
 
 export function Pools(){
+  const navigation = useNavigation()
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus Boloes" />
@@ -12,6 +14,7 @@ export function Pools(){
         <Button 
           title="Buscar bolao por codigo" 
           leftIcon={<MagnifyingGlass size={20} color="#000" weight="bold" />}
+          onPress={() => navigation.navigate('find')}
           />
       </VStack>
     </VStack>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
 import { poolRoutes } from "./routes/pool";
 import { guessRoutes } from "./routes/guess";
 import { userRoutes } from "./routes/user";
@@ -15,13 +17,17 @@ async function bootstrap(){
     origin: true
   })
 
+  await fastify.register(jwt, {
+    secret: 'nlwcopa'
+  })
+
   await fastify.register(poolRoutes)
   await fastify.register(userRoutes)
   await fastify.register(guessRoutes)
   await fastify.register(authRoutes)
   await fastify.register(gameRoutes)
 
-  await fastify.listen({ port: 3333})
+  await fastify.listen({ port: 3333, host: '0.0.0.0'})
 }
 
 bootstrap()
